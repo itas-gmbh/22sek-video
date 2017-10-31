22c1587562ef2ee3f20ad4c03306b1e37c4f4c26
itas-gmbh <info@itas.cc>
2017-10-31 16:32:52 +0100

    Update main.js

--- www/main.js
+++ www/main.js
@@ -1,10 +1,3 @@
-  
-document.addEventListener("deviceready", onDeviceReady, false);
-function onDeviceReady() {
-    console.log(navigator.device.capture);
-        alert("Device is ready");
-}    
-
 function getpage(page){
 	//alert('getpage startedt');
 	    jQuery.ajax({
@@ -118,24 +111,6 @@ function getpage(page){
 
 
 
-// capture callback
-var captureSuccess = function(mediaFiles) {
-    var i, path, len;
-    for (i = 0, len = mediaFiles.length; i < len; i += 1) {
-        path = mediaFiles[i].fullPath;
-        // do something interesting with the file
-    }
-};
-
-// capture error callback
-var captureError = function(error) {
-    navigator.notification.alert('Error code: ' + error.code, null, 'Capture Error');
-};
-
-// start video capture
-function videoCapture(){
-navigator.device.capture.captureVideo(captureSuccess, captureError, {limit:2});
-}
 
 
 
